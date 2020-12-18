7143bece8e70f90788785228cb2a003ca002d73c
prettyirrelevant <ienioladewumi@gmail.com>
2020-12-18 13:46:53 +0100

    allowed hosts

--- chat_app_project/settings/production.py
+++ chat_app_project/settings/production.py
@@ -10,4 +10,4 @@ SECRET_KEY = env("SECRET_KEY")
 
 DATABASES = {"default": env.db("DATABASE_URL")}
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=['chatapp.eniola.xyz'])
