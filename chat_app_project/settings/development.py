1960b826b8e1e7de6757e06bbab5b293725f80a6
prettyirrelevant <ienioladewumi@gmail.com>
2020-12-18 07:10:45 +0100

    refactor begin

--- chat_app_project/settings/development.py
+++ chat_app_project/settings/development.py
@@ -11,22 +11,20 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'fd2f471d4f64c134a556d8f3370e687b24ef33fa33c545733901a8395e4de8db'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["chatapp.eniola.xyz"]
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -137,23 +135,3 @@ CHANNEL_LAYERS = {
         },
     }
 }
-
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-
-AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
-
-AWS_S3_OBJECT_PARAMETERS = {
-    "CacheControl": "max-age=86400",
-}
-
-AWS_LOCATION = "static"
-
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
-DEFAULT_FILE_STORAGE = "chat_app_project.storage_backends.MediaStorage"
--- chat_app_project/storage_backends.py
+++
@@ -1,6 +0,0 @@
-from storages.backends.s3boto3 import S3Boto3Storage
-
-
-class MediaStorage(S3Boto3Storage):
-    location = "attachments"
-    file_overwrite = False
